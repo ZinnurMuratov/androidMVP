23eebf7a6ed3f154938bb7014c95fc39477d800b
Zinnur <Zinnur@biosinc.local>
2016-11-03 23:38:29 +0300

    refactoring

--- app/src/main/java/zr/reactive/zinnur/rxzr/other/Const.java
+++ app/src/main/java/zr/reactive/zinnur/rxzr/other/Const.java
@@ -8,14 +8,14 @@ public interface Const {
     String UI_THREAD = "UI_THREAD";
     String IO_THREAD = "IO_THREAD";
 
-    String BASE_URL = "https://zr.life/";
+    String BASE_URL = "";
     String DRIBBBLE_URL = "https://api.dribbble.com/v1/";
     String ClientId = "22631af64535664b714efcc0d6e3e839a9abaaffc8da19c0c1186c4a8d1ce2e2";
     String ClientSecret = "536dfc603fa00746928d02c1b7309f5004c95f593e382577f1f7ec1a94de98c0";
     String ClientAccessToken = "02403ea2aa53b805dc2e0052131d124056ab68e612c106da5869ef44fc4e80f4";
 
-    String LOGIN = "89370022713";
-    String PASSWORD = "12345678";
+    String LOGIN = "";
+    String PASSWORD = "";
 
     String JSON_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
 
